cda75fb3bfc3209791959cba6a7fdb86fb0519e8
Ananya Agarwal <80957726+ananya-codeit@users.noreply.github.com>
2021-06-08 20:57:09 +0530

    members section

--- glass cards/main.js
+++ glass cards/main.js
@@ -1,16 +1,28 @@
 const firstBtn = document.getElementById('coreBtn');
 const secondBtn = document.getElementById('exboBtn');
+const thirdBtn = document.getElementById('membersBtn');
 const coreDiv = document.getElementById('core');
 const exboDiv = document.getElementById('exbo');
+const membersDiv = document.getElementById('members');
+
 
 firstBtn.addEventListener('click', (e) => {
     e.preventDefault();
     coreDiv.style.display = "";
     exboDiv.style.display = "none";
+    members.style.display="none";
+
 });
 
 secondBtn.addEventListener('click', (e) => {
     e.preventDefault();
     coreDiv.style.display = "none";
     exboDiv.style.display = "";
+    members.style.display="none";
+});
+thirdBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    coreDiv.style.display = "none";
+    exboDiv.style.display = "none";
+    members.style.display="";
 });
